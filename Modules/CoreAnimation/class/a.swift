9e03167d4f2544e95d613b3f2ed814c17cc19751
churabou <ginga7888@gmail.com>
2018-02-17 20:19:40 +0900

    create: CoreAnimatino Folder

---
+++ Modules/CoreAnimation/class/a.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+
+class Hoge {
+    static func hoge(view: UIView) {
+        view.backgroundColor = .red
+    }
+}
---
+++ Modules/CoreAnimation/example/example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  example
+//
+//  Created by ちゅーたつ on 2018/02/17.
+//  Copyright © 2018年 ちゅーたつ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Hoge.hoge(view: view)
+    }
+}
+
